ba1e6c091b6a61d56fd2cf26207751b7dc94fbfc
Martin <karag2006@gmx.net>
2024-10-29 22:11:10 +0100

    testing api basics

--- api_stock_update.php
+++ api_stock_update.php
@@ -14,21 +14,23 @@ $filtered_list = array_filter($list, function($product) {
     return isset($product->manufacturerNumber);
 });
 
+echo "Filtered List: " . count($filtered_list) . " Products";
+
 // foreach Product in filtered List Get Prodcut Details from PT
-$count = 0;
+// $count = 0;
 
-foreach($filtered_list as $product) {
-    $ptCode = $product->manufacturerNumber;
-    $stock = $Api_pt->getProductStock($ptCode);
-    // Update Stock in all Products in Filtered List - if stock did change from last time
-    if($stock && $stock != $product->stock) {
-        // Foreach Product in Filtered List -> Patch Stock at TP24
-        $Api_tp24->setStock($product->id, $stock);
-        $count++;
-    }
-}
+// foreach($filtered_list as $product) {
+//     $ptCode = $product->manufacturerNumber;
+//     $stock = $Api_pt->getProductStock($ptCode);
+//     // Update Stock in all Products in Filtered List - if stock did change from last time
+//     if($stock && $stock != $product->stock) {
+//         // Foreach Product in Filtered List -> Patch Stock at TP24
+//         $Api_tp24->setStock($product->id, $stock);
+//         $count++;
+//     }
+// }
 
-// Notify Admin about the number of Products that had their stock updated
-echo "Updated Stock for $count Products"; // TODO: change into notification using Notify
+// // Notify Admin about the number of Products that had their stock updated
+// echo "Updated Stock for $count Products"; // TODO: change into notification using Notify
 
 ?>
