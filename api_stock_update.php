afda3fe3ba232e64aa942cb1f72d31c1629dd666
Martin Richter <karag2006@gmx.net>
2024-10-30 13:05:38 +0100

    Update api_stock_update.php

--- api_stock_update.php
+++ api_stock_update.php
@@ -15,7 +15,7 @@ $filtered_list = array_filter($list, function($product) {
 });
 
 $test = $Api_pt->getProductDetails("154.140.018.019");
-echo json_encode($test);
+echo json_encode($test->product);
 
 // foreach Product in filtered List Get Prodcut Details from PT
 /* $count = 0;
